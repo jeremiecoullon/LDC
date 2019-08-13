a9acadd442be7259184677a2e21dfaca31f55bb3
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2019-08-13 10:51:33 +0100

    allowed_hosts fix

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['pure-temple-86735.herokuapp.com', 'www.pure-temple-86735.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
