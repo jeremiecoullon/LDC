05edf5d9da18d9a449d625135e5eeab731329370
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2019-08-15 14:14:04 +0100

    fix s3 storage thing in s3

--- config/settings.py
+++ config/settings.py
@@ -209,7 +209,7 @@ if not os.path.isfile("../local.txt"):
     ]
     
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # # ===============
 
 # Activate Django-Heroku.
