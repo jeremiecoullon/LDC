e3c6a237fe82f3c4be9a150019cdf9e3b96ae1b2
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2019-09-06 19:23:16 +0100

    added thumbnails to all models

--- DjangoVerse/admin.py
+++ DjangoVerse/admin.py
@@ -7,22 +7,24 @@ from .models import Player, Band, Festival, Venue, Album, Instrument
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
 	filter_horizontal = ('band', 'instrument', 'festival',)# 'venue', 'album',)
+	exclude = ('thumbnail',)
 
 @admin.register(Band)
 class BandAdmin(admin.ModelAdmin):
 	filter_horizontal = ('festival', )#'venue', 'album',)
+	exclude = ('thumbnail',)
 
 @admin.register(Festival)
 class FestivalAdmin(admin.ModelAdmin):
-	pass
+	exclude = ('thumbnail',)
 
 # @admin.register(Venue)
 # class VenueAdmin(admin.ModelAdmin):
-# 	pass
+# 	exclude = ('thumbnail',)
 
 # @admin.register(Album)
 # class AlbumAdmin(admin.ModelAdmin):
-# 	pass
+# 	exclude = ('thumbnail',)
 
 @admin.register(Instrument)
 class InstrumentAdmin(admin.ModelAdmin):
--- DjangoVerse/models.py
+++ DjangoVerse/models.py
@@ -1,5 +1,8 @@
 import uuid
 from django.db import models
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
 from country_list import countries_for_language
 
 COUNTRY_LIST = [(k, v) for k, v in dict(countries_for_language('en')).items()]
@@ -44,24 +47,76 @@ class Instrument(models.Model):
 	def __str__(self):
 		return self.name
 
+def compress(image):
+	im = Image.open(image)
+	# create a BytesIO object
+	im = im.convert('RGB')
+	im_io = BytesIO() 
+	# save image to BytesIO object
+	im.save(im_io, 'JPEG', quality=70) 
+	# create a django-friendly Files object
+	new_image = File(im_io, name=image.name)
+	return new_image
+
 class Festival(BaseInfo):
+	# image = models.ImageField(upload_to=festival_img_path, null=True, blank=True)
 	image = models.ImageField(upload_to=festival_img_path, null=True, blank=True)
+	thumbnail = models.ImageField(upload_to=festival_img_path, null=True, blank=True)
 	isactive = models.BooleanField(default=True)
+
+	def save(self, *args, **kwargs):
+		if self.image:
+			# call the compress function
+			new_image = compress(self.image)
+			# set self.image to new_image
+			self.thumbnail = new_image
+		# save
+		super(Festival, self).save(*args, **kwargs)
 
 class Venue(BaseInfo):
 	image = models.ImageField(upload_to=venue_img_path, null=True, blank=True)
+	thumbnail = models.ImageField(upload_to=venue_img_path, null=True, blank=True)
 	isactive = models.BooleanField(default=True)
+
+	def save(self, *args, **kwargs):
+		if self.image:
+			# call the compress function
+			new_image = compress(self.image)
+			# set self.image to new_image
+			self.thumbnail = new_image
+		# save
+		super(Venue, self).save(*args, **kwargs)
 
 class Album(BaseInfo):
 	image = models.ImageField(upload_to=album_img_path, null=True, blank=True)
+	thumbnail = models.ImageField(upload_to=album_img_path, null=True, blank=True)
 	date = models.DateTimeField(null=True, blank=True)
+
+	def save(self, *args, **kwargs):
+		if self.image:
+			# call the compress function
+			new_image = compress(self.image)
+			# set self.image to new_image
+			self.thumbnail = new_image
+		# save
+		super(Venue, self).save(*args, **kwargs)
 
 class Band(BaseInfo):
 	image = models.ImageField(upload_to=band_img_path, null=True, blank=True)
+	thumbnail = models.ImageField(upload_to=band_img_path, null=True, blank=True)
 	isactive = models.BooleanField(default=True)
 	festival = models.ManyToManyField(Festival, blank=True, related_name='bandsplayed')
 	venue = models.ManyToManyField(Venue, blank=True, related_name='bandsplayed')
 	album = models.ManyToManyField(Album, blank=True, related_name='band')
+
+	def save(self, *args, **kwargs):
+		if self.image:
+			# call the compress function
+			new_image = compress(self.image)
+			# set self.image to new_image
+			self.thumbnail = new_image
+		# save
+		super(Band, self).save(*args, **kwargs)
 
 
 
@@ -70,10 +125,20 @@ class Player(BaseInfo):
 	isactive = models.BooleanField(default=True)
 	# not required
 	image = models.ImageField(upload_to=player_img_path, null=True, blank=True)
+	thumbnail = models.ImageField(upload_to=player_img_path, null=True, blank=True)
 	band = models.ManyToManyField(Band, blank=True, related_name='members')
 	festival = models.ManyToManyField(Festival, blank=True, related_name='playersplayed')
 	venue = models.ManyToManyField(Venue, blank=True, related_name='playersplayed')
 	album = models.ManyToManyField(Album, blank=True, related_name='playersplayed')
+
+	def save(self, *args, **kwargs):
+		if self.image:
+			# call the compress function
+			new_image = compress(self.image)
+			# set self.image to new_image
+			self.thumbnail = new_image
+		# save
+		super(Player, self).save(*args, **kwargs)
 	
 
 
