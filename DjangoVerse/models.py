a9cc0ffaf97e480a51536cef9592600abf9c619e
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2019-09-24 11:27:06 +0100

    DV: crop images if the width:height ratio isn't approx w/h=1.5

--- DjangoVerse/models.py
+++ DjangoVerse/models.py
@@ -48,8 +48,31 @@ class Instrument(models.Model):
 	def __str__(self):
 		return self.name
 
+
+def resize_image(image):
+	width, height = image.size
+	if abs(width/height - (1.5)) <= 0.1:
+		pass
+	elif (width/height) < 1.4:
+		# crop_height: remove an equal amount from both sides
+		left = 0
+		right = width
+		top = height/2 - (1/3)*width
+		bottom = height/2 + (1/3)*width
+		image = image.crop((left, top, right, bottom))
+	elif (width/height) > 1.6:
+		# crop width: remove an equal amount from both sides
+		left = (width/2) - (3/4)*height
+		right = (width/2) + (3/4)*height
+		top = 0
+		bottom = height
+		image = image.crop((left, top, right, bottom))
+	return image	
 def compress(image):
 	im = Image.open(image)
+	# resize image if the ratio isn't approimately width/height = 1.5:
+	im = resize_image(image=im)
+	
 	# create a BytesIO object
 	im = im.convert('RGB')
 	im_io = BytesIO() 
